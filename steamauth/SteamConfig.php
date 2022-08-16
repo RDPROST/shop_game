9bc5fd0f79cf1a3e73a3e9c60118ef03fde6d9d7
RDPROST <redoicere@gmail.com>
2022-08-16 16:00:12 +0300

    Update SteamConfig.php

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,6 +1,6 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "B2D051941A8836BB13B369850D932093"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['apikey'] = "B5CB252A5300B111F8B4770FF66FA2BB"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "https://idanamedi.fun/"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "/"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
